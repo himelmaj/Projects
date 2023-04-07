b3d39ee2f77148e1488276f9481c3e6c50bb0652
Himel Majumder <93002708+himelmaj@users.noreply.github.com>
2023-04-07 21:59:18 +0200

    Update my_ip.py

--- Python_Scripts/my_ip.py
+++ Python_Scripts/my_ip.py
@@ -1,12 +1,21 @@
 import socket as s
+import tkinter as tk
+
+root = tk.Tk()
+root.geometry('600x30')
+font = ("Helvetica", 14)
+root.resizable(False, False)
+root.title('MY IP')
+root.configure(background='black')
+
+
+# Get host information
 my_hostname = s.gethostname()
 my_ip = s.gethostbyname(my_hostname)
 
-host_name = f'Your Hostname is {my_hostname}'
-ip = f'You address is {my_ip}'
+tk.Label(root, text=f'Your Hostname is {my_hostname} and you address is {my_ip}', font=font, fg='green', bg='black', anchor='se').grid(row=0, column=0)
+root.mainloop()
 
-print(host_name)
-print(ip)
 
 
 
