15ee3eea914c56810cb3141d85137127b828e6d1
Himel Majumder <93002708+himelmaj@users.noreply.github.com>
2023-04-17 18:13:02 +0200

    Update fileOrganizer.py

--- Python_Scripts/fileOrganizer.py
+++ Python_Scripts/fileOrganizer.py
@@ -8,11 +8,11 @@ DOWNLOADS_PATH = os.path.join(
 )
 
 # File type to folder
-file_mappings = collections.defaultdict()
+file_mappings = collections.defaultdict(list)
 for filename in os.listdir(DOWNLOADS_PATH):
     if not os.path.isdir(os.path.join(DOWNLOADS_PATH, filename)):
         file_type = filename.split('.')[-1]
-        file_mappings.setdefault(file_type, []).append(filename)
+        file_mappings[file_type].append(filename)
 
 # Move all files into their folder
 for folder_name, folder_items in file_mappings.items():
@@ -23,5 +23,12 @@ for folder_name, folder_items in file_mappings.items():
     for folder_item in folder_items:
         source = os.path.join(DOWNLOADS_PATH, folder_item)
         destination = os.path.join(folder_path, folder_item)
-        print(f'Moviendo {source} a {destination}')
+        if os.path.exists(destination):
+            # Rename existing file with _old suffix
+            old_destination = os.path.join(folder_path, folder_item.split('.')[0] + '_old.' + folder_item.split('.')[1])
+            os.rename(destination, old_destination)
+            print(f'Renamed existing file to {old_destination}')
+
+        print(f'Moving {source} to {destination}')
         os.rename(source, destination)
+
